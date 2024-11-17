0e0f3ced775fb4e8a6be9f4193421bfccde1088b
Arpit kumar Kanwar <118201864+arpitkumarfault@users.noreply.github.com>
2024-11-17 11:17:46 +0530

    Update vite.config.js
    add deploye link

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
   proxy:{
-    "/api":"http://localhost:7000",
+    "/api":"https://test-deploy-backend-kfsw.onrender.com",
   }
   },
   plugins: [react()],
